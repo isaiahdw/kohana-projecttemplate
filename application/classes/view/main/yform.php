ce06a9061345c42ec057c4f6a4845ccdc798de4a
Lorenzo Pisani <zeelot3k@gmail.com>
2010-08-11 18:02:44 -0700

    some more little code snippets

--- application/classes/view/main/yform.php
+++ application/classes/view/main/yform.php
@@ -7,12 +7,22 @@ class View_Main_Yform extends View_Website
 	public function form()
 	{
 		$yf = YForm::factory()
-			->add_values((array)$this->values);
+			->add_values((array)$this->values)
+			->add_messages('errors', array
+			(
+				'checkboxes' => 'some error',
+				'radios' => 'another error',
+			));
 
 		return array
 		(
 			array('element' => $yf->open()),
 			array('element' => $yf->text('text')),
+			array('element' => $yf->checkboxGroup('checkboxes')
+				->add_options(array('one' => 'hello', 'two' => 'world'))),
+			
+			array('element' => $yf->radioGroup('radios')
+				->add_options(array('one' => 'hello', 'two' => 'world'))),
 			array('element' => $yf->submit('submit')),
 			array('element' => $yf->close()),
 		);
