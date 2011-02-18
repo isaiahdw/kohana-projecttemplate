1f56e07b30d75d15609b90cef714f3458cd68bd4
Bob Eagan <bob@synapsestudios.com>
2011-02-18 14:43:58 -0700

    making unittest config use blacklist instead of whitelist so that broken userguide is skipped

--- application/config/unittest.php
+++ application/config/unittest.php
@@ -4,13 +4,17 @@ return array(
 	'environment'    => Kohana::TESTING,
 	'temp_path'      => Kohana::$cache_dir.'/unittest',
 	'cc_report_path' => 'report',
-	'use_whitelist'  => TRUE,
+	'use_whitelist'  => FALSE,
 	'whitelist'      => array(
 		'app'        => TRUE,
 		'modules'    => array(TRUE),
 		'system'     => TRUE,
 	),
-	'use_blacklist' => FALSE,
-	'blacklist' => array(),
+	'use_blacklist' => TRUE,
+	'blacklist'      => array(
+		'app'        => FALSE,
+		'modules'    => array('userguide'),
+		'system'     => FALSE,
+	),
 	'db_connection' => 'default',
 );
