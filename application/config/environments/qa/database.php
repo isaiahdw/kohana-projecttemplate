761402b85a830e7468198ddd3d2d322a785c75b0
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-31 16:59:01 -0700

    adding a qa db config

---
+++ application/config/environments/qa/database.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+	'default' => array(
+		'type'       => 'mysql',
+		'connection' => array(
+			// @TODO: change this to be project-specific
+			'hostname'   => 'localhost',
+			'username'   => 'projecttemplate',
+			'password'   => 'projecttemplate',
+			'database'   => 'projecttemplate_qa',
+			'persistent' => FALSE,
+		),
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => FALSE,
+		'profiling'    => TRUE,
+	),
+);
